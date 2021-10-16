aec954a0c29493356649b117646e90bc2dac4b37
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2021-10-16 14:13:18 -0700

    added more ideas to the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,27 @@
 //do current weather to get lat and long
 //then use latitude and logitude to use the one call api to get more info
 // fire off same chunk of code for the search or if they click the saved smacks of cities below
+
+function mainWeather() {
+
+    //put fetch here
+
+    var lat = data.coord.lat
+    var long = data.coord.long
+
+}
+
+function getOneCall() {
+
+    //var url = new one call api url with lat lon added
+    //put fetch here
+
+
+}
+
+function displayData() {
+
+    //display on page
+    //1, 2, 3 punch
+
+}
