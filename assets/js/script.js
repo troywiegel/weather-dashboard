cce926102f6f0d2f4f5c92aac40429fdbea10985
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2021-10-16 14:05:06 -0700

    added some html and pseudo code ideas to the js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Stuff to think about to start the project
+
+//openweathermap.org/api to get current weather information
+//have to sign up to get an api key
+//need one api smack to get the current weather
+//need another api smack to get forecast
+//do current weather to get lat and long
+//then use latitude and logitude to use the one call api to get more info
+// fire off same chunk of code for the search or if they click the saved smacks of cities below
