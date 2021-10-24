3b1b4cb8cee6afe846225c6f7c6e0c63bd7e50de
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2021-10-24 15:16:38 -0700

    added weather icons, an image and updated the readme file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# weather-dashboard
+# Weather Dashboard
+
+I created a weather dashboard that:
+
+* Allows a user to input a desired city
+* That city's current weather and a 5 day forecast appear on the viewport
+* The searched cities are stored to local storage and placed on the viewport underneath the search bar
+-----------------------------------------------------------------------------------------------------------------
+
+
+Website Link: https://troywiegel.github.io/weather-dashboard
+
+Website Screenshot: 
+
+![Weather Dashboard Screenshot 1](https://troywiegel.github.io/weather-dashboard/assets/images/wd.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ $('#searchBtn').on('click', function () {
     $.ajax({
         url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city.val() + '&appid=14a17e4b1eca2f426f4a1fdcd85e2f0f'
     }).then(function (data) {
+        console.log('data??', data)
         let name = data.name
         let icon = data.weather[0].icon
         let lat = data.coord.lat
@@ -22,7 +23,7 @@ function getOneCall(name, icon, lat, lon) {
     $.ajax({
         url: 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&units=imperial&appid=14a17e4b1eca2f426f4a1fdcd85e2f0f'
     }).then(function (data) {
-
+        
         let city = name
         let weatherIcon = icon
         let temp = data.current.temp
@@ -40,153 +41,106 @@ function displayDaily(city, weatherIcon, temp, wind, humidity, uvi) {
 
     $('#dailyWeather').empty()
 
+
     let cityName = $('<div>')
     let date = moment().format('MMM Do YYYY')
     $(cityName).attr('id', 'cityName')
-    // let cityImage = $('<img>')
-    // $(cityImage).attr('src', ('https://openweathermap.org/img/w/' + weatherIcon + '.png'))
     $(cityName).html(city + ' | ' + date)
     $('#dailyWeather').append(cityName)
+    $('#dailyIcon').attr('src', 'https://openweathermap.org/img/wn/' + weatherIcon + '.png')
 
-    let cityTemp = $('<li>')
-    $(cityTemp).html('Temp: ' + temp + '\xB0' + 'F')
-    $('#dailyWeather').append(cityTemp)
-
-    let cityWind = $('<li>')
-    $(cityWind).html('Wind: ' + wind + ' MPH')
-    $('#dailyWeather').append(cityWind)
-
-    let cityHumid = $('<li>')
-    $(cityHumid).html('Humidity: ' + humidity + ' %')
-    $(dailyWeather).append(cityHumid)
-
-    let cityUvi = $('<li>')
-    $(cityUvi).html('UV Index: ' + uvi)
-    $('#dailyWeather').append(cityUvi)
+    $('#cityTemp').html('Temp: ' + temp + '\xB0' + 'F')
+    $('#cityWind').html('Wind: ' + wind + ' MPH')
+    $('#cityHumid').html('Humidity: ' + humidity + ' %')
+    $('#cityUvi').html('UV Index: ' + uvi)
 }
 
 function displayWeekly(data) {
 
     $('.empty').empty()
 
-    // let forecast = [
-
-    //     {
-    //         // date: ,
-    //         // image: ,
-    //         temp: data.daily[0].temp.day,
-    //         wind: data.daily[0].wind_speed,
-    //         humidity: data.daily[0].humidity
-
-    //     },
-    //     {
-    //         // date: ,
-    //         // image: ,
-    //         temp: data.daily[1].temp.day,
-    //         wind: data.daily[1].wind_speed,
-    //         humidity: data.daily[1].humidity
-
-    //     },
-    //     {
-    //         // date: ,
-    //         // image: ,
-    //         temp: data.daily[2].temp.day,
-    //         wind: data.daily[2].wind_speed,
-    //         humidity: data.daily[2].humidity
-
-    //     },
-    //     {
-    //         // date: ,
-    //         // image: ,
-    //         temp: data.daily[3].temp.day,
-    //         wind: data.daily[3].wind_speed,
-    //         humidity: data.daily[3].humidity
-
-    //     },
-    //     {
-    //         // date: ,
-    //         // image: ,
-    //         temp: data.daily[4].temp.day,
-    //         wind: data.daily[4].wind_speed,
-    //         humidity: data.daily[4].humidity
-
-    //     },
-    // ]
-
-    // for (let i = 0; i < forecast.length; i++) {
-
-    //     let weeklyForecast = $('<div>')
-    //     $(weeklyForecast).html(forecast[i])
-    //     $('#weeklyWeather').append(weeklyForecast)
-
-    // }
     let weeklyTemp1 = data.daily[0].temp.day
     let weeklyWind1 = data.daily[0].wind_speed
     let weeklyHumidity1 = data.daily[0].humidity
+    let weeklyIcon1 = data.daily[0].weather[0].icon
 
-    let day1Date = ('<li>' + moment().add(1, 'days').format('MMM Do YYYY') + '<li>')
-    let day1Temp = ('<li>' + 'Temp: ' + weeklyTemp1 + '\xB0' + 'F' + '<li>')
-    let day1Wind = ('<li>' + 'Wind: ' + weeklyWind1 + ' MPH' + '<li>')
-    let day1Humidity = ('<li>' + 'Humidity: ' + weeklyHumidity1 + '%' + '<li>')
-    $('#day1').append(day1Date)
-    $('#day1').append(day1Temp)
-    $('#day1').append(day1Wind)
-    $('#day1').append(day1Humidity)
+    let day1Date = moment().add(1, 'days').format('MMM Do YYYY')
+    let day1Temp = 'Temp: ' + weeklyTemp1 + '\xB0' + 'F'
+    let day1Wind = 'Wind: ' + weeklyWind1 + ' MPH'
+    let day1Humidity = 'Humidity: ' + weeklyHumidity1 + '%'
+    
+    $('#day1Date').text(day1Date)
+    $('#day1Temp').text(day1Temp)
+    $('#day1Wind').text(day1Wind)
+    $('#day1Humidity').text(day1Humidity)
+    $('#weeklyIcon1').attr("src", "https://openweathermap.org/img/wn/" + weeklyIcon1 + ".png")
+    $('#weeklyIcon1').attr("alt", data.daily[0].weather[0].description)
 
     let weeklyTemp2 = data.daily[1].temp.day
     let weeklyWind2 = data.daily[1].wind_speed
     let weeklyHumidity2 = data.daily[1].humidity
+    let weeklyIcon2 = data.daily[1].weather[0].icon
 
-    let day2Date = ('<li>' + moment().add(2, 'days').format('MMM Do YYYY') + '<li>')
-    let day2Temp = ('<li>' + 'Temp: ' + weeklyTemp2 + '\xB0' + 'F' + '<li>')
-    let day2Wind = ('<li>' + 'Wind: ' + weeklyWind2 + ' MPH' + '<li>')
-    let day2Humidity = ('<li>' + 'Humidity: ' + weeklyHumidity2 + '%' + '<li>')
-    $('#day2').append(day2Date)
-    $('#day2').append(day2Temp)
-    $('#day2').append(day2Wind)
-    $('#day2').append(day2Humidity)
+    let day2Date = moment().add(2, 'days').format('MMM Do YYYY')
+    let day2Temp = 'Temp: ' + weeklyTemp2 + '\xB0' + 'F'
+    let day2Wind = 'Wind: ' + weeklyWind2 + ' MPH'
+    let day2Humidity = 'Humidity: ' + weeklyHumidity2 + '%'
+    $('#day2Date').append(day2Date)
+    $('#day2Temp').append(day2Temp)
+    $('#day2Wind').append(day2Wind)
+    $('#day2Humidity').append(day2Humidity)
+    $('#weeklyIcon2').attr("src", "https://openweathermap.org/img/wn/" + weeklyIcon2 + ".png")
+    $('#weeklyIcon2').attr("alt", data.daily[1].weather[0].description)
 
     let weeklyTemp3 = data.daily[2].temp.day
     let weeklyWind3 = data.daily[2].wind_speed
     let weeklyHumidity3 = data.daily[2].humidity
+    let weeklyIcon3 = data.daily[2].weather[0].icon
 
-    let day3Date = ('<li>' + moment().add(3, 'days').format('MMM Do YYYY') + '<li>')
-    let day3Temp = ('<li>' + 'Temp: ' + weeklyTemp3 + '\xB0' + 'F' + '<li>')
-    let day3Wind = ('<li>' + 'Wind: ' + weeklyWind3 + ' MPH' + '<li>')
-    let day3Humidity = ('<li>' + 'Humidity: ' + weeklyHumidity3 + '%' + '<li>')
-    $('#day3').append(day3Date)
-    $('#day3').append(day3Temp)
-    $('#day3').append(day3Wind)
-    $('#day3').append(day3Humidity)
+    let day3Date = moment().add(3, 'days').format('MMM Do YYYY')
+    let day3Temp = 'Temp: ' + weeklyTemp3 + '\xB0' + 'F'
+    let day3Wind = 'Wind: ' + weeklyWind3 + ' MPH'
+    let day3Humidity = 'Humidity: ' + weeklyHumidity3 + '%'
+    $('#day3Date').append(day3Date)
+    $('#day3Temp').append(day3Temp)
+    $('#day3Wind').append(day3Wind)
+    $('#day3Humidity').append(day3Humidity)
+    $('#weeklyIcon3').attr("src", "https://openweathermap.org/img/wn/" + weeklyIcon3 + ".png")
+    $('#weeklyIcon3').attr("alt", data.daily[2].weather[0].description)
 
     let weeklyTemp4 = data.daily[3].temp.day
     let weeklyWind4 = data.daily[3].wind_speed
     let weeklyHumidity4 = data.daily[3].humidity
+    let weeklyIcon4 = data.daily[3].weather[0].icon
 
-    let day4Date = ('<li>' + moment().add(4, 'days').format('MMM Do YYYY') + '<li>')
-    let day4Temp = ('<li>' + 'Temp: ' + weeklyTemp4 + '\xB0' + 'F' + '<li>')
-    let day4Wind = ('<li>' + 'Wind: ' + weeklyWind4 + ' MPH' + '<li>')
-    let day4Humidity = ('<li>' + 'Humidity: ' + weeklyHumidity4 + '%' + '<li>')
-    $('#day4').append(day4Date)
-    $('#day4').append(day4Temp)
-    $('#day4').append(day4Wind)
-    $('#day4').append(day4Humidity)
+    let day4Date = moment().add(4, 'days').format('MMM Do YYYY')
+    let day4Temp = 'Temp: ' + weeklyTemp4 + '\xB0' + 'F'
+    let day4Wind = 'Wind: ' + weeklyWind4 + ' MPH'
+    let day4Humidity = 'Humidity: ' + weeklyHumidity4 + '%'
+    $('#day4Date').append(day4Date)
+    $('#day4Temp').append(day4Temp)
+    $('#day4Wind').append(day4Wind)
+    $('#day4Humidity').append(day4Humidity)
+    $('#weeklyIcon4').attr("src", "https://openweathermap.org/img/wn/" + weeklyIcon4 + ".png")
+    $('#weeklyIcon4').attr("alt", data.daily[3].weather[0].description)
 
     let weeklyTemp5 = data.daily[4].temp.day
     let weeklyWind5 = data.daily[4].wind_speed
     let weeklyHumidity5 = data.daily[4].humidity
+    let weeklyIcon5 = data.daily[4].weather[0].icon
 
-    let day5Date = ('<li>' + moment().add(5, 'days').format('MMM Do YYYY') + '<li>')
-    let day5Temp = ('<li>' + 'Temp: ' + weeklyTemp5 + '\xB0' + 'F' + '<li>')
-    let day5Wind = ('<li>' + 'Wind: ' + weeklyWind5 + ' MPH' + '<li>')
-    let day5Humidity = ('<li>' + 'Humidity: ' + weeklyHumidity5 + '%' + '<li>')
-    $('#day5').append(day5Date)
-    $('#day5').append(day5Temp)
-    $('#day5').append(day5Wind)
-    $('#day5').append(day5Humidity)
+    let day5Date = moment().add(5, 'days').format('MMM Do YYYY')
+    let day5Temp = 'Temp: ' + weeklyTemp5 + '\xB0' + 'F'
+    let day5Wind = 'Wind: ' + weeklyWind5 + ' MPH'
+    let day5Humidity = 'Humidity: ' + weeklyHumidity5 + '%'
+    $('#day5Date').append(day5Date)
+    $('#day5Temp').append(day5Temp)
+    $('#day5Wind').append(day5Wind)
+    $('#day5Humidity').append(day5Humidity)
+    $('#weeklyIcon5').attr("src", "https://openweathermap.org/img/wn/" + weeklyIcon5 + ".png")
+    $('#weeklyIcon5').attr("alt", data.daily[4].weather[0].description)
 
     storage()
-
 }
 
 function storage() {
